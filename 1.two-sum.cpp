12906f63b62653cf80fcb119ba31b246ff5dfac6
fanliyong007 <fanliyong007@hotmail.com>
2020-01-13 23:51:19 +0800

    vector部分完成

--- 1.two-sum.cpp
+++ 1.two-sum.cpp
@@ -7,7 +7,15 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> tmp;
-        tmp.insert(nums[0]+nums[1]);
+        for(auto i:nums)
+        {
+            need_index=find(nums.begin(),nums.end(),targe-i);
+            if(need_index!= -1)
+            {
+                tmp.push_back(i);
+                tmp.push_back(nums[need_index]);
+            }
+        }
         return tmp;
     }
 };
